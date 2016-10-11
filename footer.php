796caecbe6c54cef627c1dfcf7caf59975247b4f
Ralph John Galindo <ralphjesy@gmail.com>
2016-10-12 07:33:01 +0800

    Adding Footer , Contact Us , About Us

--- footer.php
+++ footer.php
@@ -1,4 +1,63 @@
 
+
+<footer id="footer">
+    <div class="container padding-top-30 margin-bottom-30">
+        <?php
+        $_menu = wp_nav_menu(array(
+            'menu' => 'Footer',
+            'walker' => new xyren_smarty_walker_nav_menu(),
+            'menu_id'=>'footerMain',
+            'container' =>'ul',
+            'menu_class' =>'list-inline text-center pull-right uppercase',
+        ));
+        ?>
+        <div class="row footer-branding">
+            <div class="col-md-4 margin-bottom-10">
+                <img src="<?=get_stylesheet_directory_uri()?>/assets/img/Logo-JCI-Imus-Wagayway-500-white.png" alt="" class="custom-logo height-100">
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <div class="container uppercase">
+            <div class="pull-right clearfix">
+                <a href="#" class="social-icon social-icon-round social-icon-light social-facebook pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook">
+                    <i class="icon-facebook"></i>
+                    <i class="icon-facebook"></i>
+                </a>
+
+                <a href="#" class="social-icon social-icon-round social-icon-light social-twitter pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter">
+                    <i class="icon-twitter"></i>
+                    <i class="icon-twitter"></i>
+                </a>
+
+                <a href="#" class="social-icon social-icon-round social-icon-light social-gplus pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google plus">
+                    <i class="icon-gplus"></i>
+                    <i class="icon-gplus"></i>
+                </a>
+
+                <a href="#" class="social-icon social-icon-round social-icon-light social-instagram pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram">
+                    <i class="icon-instagram"></i>
+                    <i class="icon-instagram"></i>
+                </a>
+
+                <a href="#" class="social-icon social-icon-round social-icon-light social-youtube pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Youtube">
+                    <i class="icon-youtube"></i>
+                    <i class="icon-youtube"></i>
+                </a>
+
+                <a href="#" class="social-icon social-icon-round social-icon-light social-rss pull-left" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rss">
+                    <i class="icon-rss"></i>
+                    <i class="icon-rss"></i>
+                </a>
+            </div>
+            <p style=" font-weight: bold; color: #fff; padding: 10px 0; margin: 0; ">
+            &copy; 2016, JCI Imus Wagayway . All Rights Reserved.
+        </p>
+    </div>
+</div>
+
+</footer>
 </div>
 <!-- /wrapper -->
 
---
+++ partials/content-home.php
@@ -0,0 +1,124 @@
+<section id="about" class="padding-xxs">
+    <div class="container">
+        <div class="heading-title heading-dotted text-center">
+            <h3>ABOUT JCI <span>IMUS WAGAYWAY</span></h3>
+        </div>
+        <div class="row">
+
+            <!-- tabs -->
+            <div class="col-md-3 col-sm-3 nopadding">
+                <ul class="nav nav-tabs nav-stacked">
+                    <li class="active">
+                        <a href="#tab_a" data-toggle="tab" class="text-right uppercase">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tab_b" data-toggle="tab" class="text-right uppercase">
+                            Our History
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#tab_c" data-toggle="tab" class="text-right uppercase">
+                            Mission and Vision
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- tabs content -->
+            <div class="col-md-9 col-sm-9 nopadding">
+                <div class="tab-content tab-stacked padding-top-0">
+                    <div id="tab_a" class="tab-pane active">
+                        <p><b>The first Junior Chamber was organized in St. Louis, Missouri, U.S.A. on October 13, 1915. On December 11, 1944, representatives from eight (8) nations met in Mexico City and the world organization-Junior Chamber International was founded. </b></p>
+                        <p>
+                            In 1974, the movement crossed the Pacific and the Manila Jaycees was founded with the approval of its Constitution on December 20, 1947. By 1948, eleven new chapters were organized in the following key cities: Cebu, San Pablo, Bacolod, Zamboanga , Davao, iloilo, Lucena, Cabanatuan, Capiz, Cavite and Tacloban.
+                        </p>
+
+                        <p>
+                            The first national convention was held February 11-13, 1949 at the Manila Hotel. The former Hon. Ramon V. del Rosario, Sr. was elected the first National President.
+                        </p>
+                    </div>
+
+                    <div id="tab_b" class="tab-pane">
+                        <p><b>The second Junior Chamber was organized in St. Louis, Missouri, U.S.A. on October 13, 1915. On December 11, 1944, representatives from eight (8) nations met in Mexico City and the world organization-Junior Chamber International was founded. </b></p>
+                        <p>
+                            In 1974, the movement crossed the Pacific and the Manila Jaycees was founded with the approval of its Constitution on December 20, 1947. By 1948, eleven new chapters were organized in the following key cities: Cebu, San Pablo, Bacolod, Zamboanga , Davao, iloilo, Lucena, Cabanatuan, Capiz, Cavite and Tacloban.
+                        </p>
+
+                        <p>
+                            The first national convention was held February 11-13, 1949 at the Manila Hotel. The former Hon. Ramon V. del Rosario, Sr. was elected the first National President.
+                        </p>
+                    </div>
+
+                    <div id="tab_c" class="tab-pane">
+                        <p><b>The third Junior Chamber was organized in St. Louis, Missouri, U.S.A. on October 13, 1915. On December 11, 1944, representatives from eight (8) nations met in Mexico City and the world organization-Junior Chamber International was founded. </b></p>
+                        <p>
+                            In 1974, the movement crossed the Pacific and the Manila Jaycees was founded with the approval of its Constitution on December 20, 1947. By 1948, eleven new chapters were organized in the following key cities: Cebu, San Pablo, Bacolod, Zamboanga , Davao, iloilo, Lucena, Cabanatuan, Capiz, Cavite and Tacloban.
+                        </p>
+
+                        <p>
+                            The first national convention was held February 11-13, 1949 at the Manila Hotel. The former Hon. Ramon V. del Rosario, Sr. was elected the first National President.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <p class="text-center">
+                    <button type="button" class="btn btn-default btn-readmore uppercase size-12">Read more <i class="fa fa-fw fa-angle-double-right size-16 nopadding"></i></button>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="padding-xxs alternate">
+    <div class="container">
+        <div class="row">
+            <div class="heading-title heading-dotted text-center">
+                <h3>OUR <span>PROJECTS</span></h3>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="dark" style="background: url('<?=get_stylesheet_directory_uri()?>/assets/img/contact-us.png')">
+    <div class="container">
+
+        <p class="text-center size-16 width-600" style="margin:0 auto;">
+            We are always ready to help you with any possible question, problem or inquiry. Connect with us thru the most popular networks
+
+        </p>
+        <div class="clearfix margin-top-20" style="margin: 0 auto; display: block; width: 500px; text-align: center;">
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-facebook" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook">
+                <i class="icon-facebook"></i>
+                <i class="icon-facebook"></i>
+            </a>
+
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-twitter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter">
+                <i class="icon-twitter"></i>
+                <i class="icon-twitter"></i>
+            </a>
+
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-gplus" data-toggle="tooltip" data-placement="top" title="" data-original-title="Google plus">
+                <i class="icon-gplus"></i>
+                <i class="icon-gplus"></i>
+            </a>
+
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-instagram" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram">
+                <i class="icon-instagram"></i>
+                <i class="icon-instagram"></i>
+            </a>
+
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-youtube" data-toggle="tooltip" data-placement="top" title="" data-original-title="Youtube">
+                <i class="icon-youtube"></i>
+                <i class="icon-youtube"></i>
+            </a>
+
+            <a href="#" class="social-icon social-icon-sm social-icon-round social-rss" data-toggle="tooltip" data-placement="top" title="" data-original-title="Rss">
+                <i class="icon-rss"></i>
+                <i class="icon-rss"></i>
+            </a>
+        </div>
+        <p class="text-center">OR</p>
+        <button class="btn btn-warning btn-orange noradius block" style="margin: auto; background: #f47423; border: 0;"><i class="fa fa-plane"></i>LEAVE US MESSAGE</button>
+    </div>
+</section>
